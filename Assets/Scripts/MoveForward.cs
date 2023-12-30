3aa9f785760057b3c6ff5e3ac5dd153cdf898102
ScarLLD <www.olegborisov1@gmail.com>
2023-12-31 02:58:38 +0300

    Update MoveForward.cs

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
-{  
+{
+    private float _stepLength = 0.005f;
+
     private void Update()
     {
         var nextPostion = transform.position;
-        nextPostion.x += 0.005f;
+        nextPostion.x += _stepLength;
         transform.position = nextPostion;
     }
 }
